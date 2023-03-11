1a2f0430c7bfb29e785ab5742dfec1d1d5659fa6
pkrukowski1 <patryk.krukowski01@gmail.com>
2023-03-11 17:21:49 +0100

    De Boor's algorithm implemented

--- BSpline_tests.py
+++ BSpline_tests.py
@@ -5,16 +5,16 @@ from torch.functional import F
 import scipy.interpolate as spi
 
 
-bs = B_Splines(torch.linspace(0,1,1000), 2)
-opt = torch.optim.Adam(bs.parameters(), lr=0.001)
+# bs = B_Splines(torch.linspace(0,1,1000), 2)
+# opt = torch.optim.Adam(bs.parameters(), lr=0.001)
 
-dtype = torch.float
-device = torch.device("cpu")
-x = torch.Tensor([0.33, 0.5, 0.774, 0.97])
-t = torch.Tensor([0.33, 0.5, 0.774, 0.97])
-print(bs.calculate_BSpline_2D_deriv_dt(x, t))
+# dtype = torch.float
+# device = torch.device("cpu")
+# x = torch.Tensor([0.33, 0.5, 0.774, 0.97])
+# t = torch.Tensor([0.33, 0.5, 0.774, 0.97])
+# print(bs.calculate_BSpline_2D_deriv_dt(x, t))
 # GT data
-y = torch.sin(x)
+# y = torch.sin(x)
 # print(bs.calculate_BSpline_1D(x))
 # print(spi.splev(x.detach(), (torch.linspace(0,1,100).detach(), bs.coefs.detach(), 2)))
 
@@ -42,3 +42,9 @@ y = torch.sin(x)
 # print(bs.coefs.grad)
 # print(x.grad)
 
+bs = B_Splines(
+    knot_vector=torch.linspace(0, 1, 10000),
+    degree=3,
+)
+x = torch.Tensor([0.1, 0.323, 0.6, 0.98])
+print(bs.calculate_BSpline_1D_deriv_dx(x, mode='Adam'))
