c1eb1aaf677ee311b0b2f125d910b915b0805439
Maciej Sikora <maciejsikora2302@gmail.com>
2023-03-29 07:59:30 +0200

    first tests

--- main_solution.py
+++ main_solution.py
@@ -267,8 +267,8 @@ if __name__ == "__main__":
         for loss_fn, name in \
             [
                 # (loss_fn_weak, 'loss_fn_weak'),
-                # (loss_fn_strong, 'loss_fn_strong'), 
-                (loss_fn_weak_and_strong, 'loss_fn_weak_and_strong'), 
+                (loss_fn_strong, 'loss_fn_strong'), 
+                # (loss_fn_weak_and_strong, 'loss_fn_weak_and_strong'), 
                 # (loss_fn_colocation, 'loss_fn_colocation')
             ]:
             logger.info(f"Training {'PINN' if not USE_SPLINE else 'splines'} for {Color.YELLOW}{EPOCHS}{Color.RESET} epochs using {Color.YELLOW}{name}{Color.RESET} loss function")
