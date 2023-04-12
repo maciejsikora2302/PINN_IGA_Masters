39102f71035ebcbdc142089ff05ed519f3888aa8
Maciej Sikora <maciejsikora2302@gmail.com>
2023-04-12 22:54:57 +0200

    initial refactor

--- general_parameters.py
+++ general_parameters.py
@@ -38,85 +38,36 @@ class Color:
 
 
 class GeneralParameters:
-    def __init__(self, \
-            length = None, \
-            total_time = None, \
-            n_points_x = None, \
-            n_points_t = None, \
-            n_points_init = None, \
-            weight_interior = None, \
-            weight_initial = None, \
-            weight_boundary = None, \
-            layers = None, \
-            neurons_per_layer = None, \
-            epochs = None, \
-            learning_rate = None, \
-            eps_interior = None, \
-            spline_degree = None, \
-            save = None, \
-            one_dimension = None, \
-            uneven_distribution = None, \
-            splines = None, \
-            pinn_is_solution = None, \
-            pinn_learns_coeff = None, \
-            optimize_test_function = None, \
-            epsilon_list = None, \
-            device = None):
-        
-        self.length = 1. if length is None else length
-        self.total_time = 1. if total_time is None else total_time
-        self.n_points_x = 100 if n_points_x is None else n_points_x
-        self.n_points_t = 150 if n_points_t is None else n_points_t
-        self.n_points_init = 300 if n_points_init is None else n_points_init
-        self.weight_interior = 50.0 if weight_interior is None else weight_interior
-        self.weight_initial = .5 if weight_initial is None else weight_initial
-        self.weight_boundary = 5.0 if weight_boundary is None else weight_boundary
-        self.layers = 4 if layers is None else layers
-        self.neurons_per_layer = 20 if neurons_per_layer is None else neurons_per_layer
-        self.epochs = 50_000 if epochs is None else epochs
-        self.learning_rate = 0.0025 if learning_rate is None else learning_rate
-        self.eps_interior = 1e-1 if eps_interior is None else eps_interior
-        self.spline_degree = 3 if spline_degree is None else spline_degree
-        self.save = False if save is None else save
-        self.one_dimension = False if one_dimension is None else one_dimension
-        self.uneven_distribution = False if uneven_distribution is None else uneven_distribution
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
-        self.splines = False if splines is None else splines
-        self.pinn_is_solution = False if pinn_is_solution is None else pinn_is_solution
-        self.pinn_learns_coeff = False if pinn_learns_coeff is None else pinn_learns_coeff
-        self.optimize_test_function = False if optimize_test_function is None else optimize_test_function
-        self.epsilon_list = torch.linspace(0.01, 0.1, 10, requires_grad=True).unsqueeze(0).mT.cuda() if epsilon_list is None else epsilon_list
-    
+    def __init__(self, args):
+        self.length = args.length
+        self.total_time = args.total_time
+        self.n_points_x = args.n_points_x
+        self.n_points_t = args.n_points_t
+        self.n_points_init = args.n_points_init
+        self.weight_interior = args.weight_interior
+        self.weight_initial = args.weight_initial
+        self.weight_boundary = args.weight_boundary
+        self.layers = args.layers
+        self.neurons_per_layer = args.neurons_per_layer
+        self.epochs = args.epochs
+        self.learning_rate = args.learning_rate
+        self.eps_interior = args.eps_interior
+        self.spline_degree = args.spline_degree
+        self.save = args.save
+        self.one_dimension = args.one_dimension
+        self.uneven_distribution = args.uneven_distribution
+        self.device = args.device
+        self.splines = args.splines
+        self.pinn_is_solution = args.pinn_is_solution
+        self.pinn_learns_coeff = args.pinn_learns_coeff
+        self.optimize_test_function = args.optimize_test_function
+        self.epsilon_list = args.epsilon_list
+
     def precalculate(self):
-
-
         if self.pinn_is_solution or self.splines or self.pinn_learns_coeff:
             self.knot_vector = torch.linspace(0, 1, self.n_points_x)
             self.knot_vector = torch.cat((torch.zeros(self.spline_degree-1), self.knot_vector, torch.ones(self.spline_degree-1)))
             self.n_coeff = len(self.knot_vector) - self.spline_degree - 1
 
-
-        # elif self.pinn_learns_coeff:
-        #     self.n_edge_knots = self.spline_degree * 2
-        #     self.n_internal_knots = self.n_points_x - self.n_edge_knots
-        #     self.n_coefs = self.n_internal_knots + self.spline_degree - 1
-
-        #     if general_parameters.uneven_distribution:
-        #         internal_knots = get_unequaly_distribution_points(eps=self.eps_interior, density_range=0.2, n=self.n_internal_knots, device='cpu')
-        #     else:
-        #         internal_knots = torch.linspace(0, 1, self.n_internal_knots)
-
-        #     self.knot_vector = torch.cat((torch.zeros(self.spline_degree-1), 
-        #                                 internal_knots,
-        #                                 torch.ones(self.spline_degree-1)))
-
-            # self.knot_vector = torch.linspace(0, 1, self.n_points_x)
-            # self.knot_vector = torch.cat((torch.zeros(self.spline_degree-1), 
-            #                             self.knot_vector[self.spline_degree-1:self.n_points_x-(self.spline_degree-1)],
-            #                             torch.ones(self.spline_degree-1)))
             
-                
-
-    
-
 general_parameters = GeneralParameters()
