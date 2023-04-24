ca21fac68a6680c44e4a6c8276e034572af120de
pkrukowski1 <patryk.krukowski01@gmail.com>
2023-04-24 22:08:14 +0200

    Splines 2D are calculated correctly right now

---
+++ Splines_2D_tests.py
@@ -0,0 +1,23 @@
+from general_parameters import GeneralParameters
+from B_Splines import B_Splines
+import torch
+
+
+parameters = GeneralParameters(None)
+parameters.pinn_is_solution = True
+parameters.n_points_x = 10
+parameters.spline_degree = 2
+parameters.optimize_test_function = True
+parameters.precalculate()
+
+knot_vector = parameters.knot_vector
+
+bs = B_Splines(
+    knot_vector=knot_vector,
+    degree=2,
+    dims=2
+)
+x=torch.linspace(0,1,5)
+t=torch.linspace(0,1,5)
+print(bs.calculate_BSpline_2D(x,t, mode='NN'))
+print(bs.calculate_BSpline_2D(x,t,mode='Adam'))
