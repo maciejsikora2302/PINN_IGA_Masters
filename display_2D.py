93ec3e1edf8247accfa71ab10c12d5a76c369a15
Patryk Krukowski <patryk.krukowski01@gmail.com>
2023-06-15 20:12:32 +0200

    Add very ugly (but working) script to generate plots of optimal test functions

--- display_2D.py
+++ display_2D.py
@@ -1,85 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import sys
 
-files_with_display_paths = [
-    "pinn_vpinn_tests_2D.txt",
-    "pinn_vpinn_tests_uneven_2D.txt",
-    "pinn_vpinn_tests_uneven_coef_opt.txt"
-]
+if __name__ == '__main__':
 
-for file_with_display_path in files_with_display_paths:
-    for loss_name in [
-            "loss_fn_basic",
-            "loss_fn_strong",
-            "loss_fn_weak",
-            "loss_fn_weak_and_strong"
-        ]:
-        with open(f"./display_paths/{file_with_display_path}") as file1:
-            file = file1.read()
-            
-            file = file.replace("\\","/")
-            files = file.split('\n')
-            for file in files:
-                pinn_values_name = os.path.join(file, loss_name, "pinn_values.txt")
-                domain_name = os.path.join(file, loss_name, "x.txt")
-                time = os.path.join(file, loss_name, "time.txt")
-                parameters = os.path.join(file, loss_name, "other_parameters.txt")
+    files_with_display_paths = [
+        "optimal_test_functions.txt"
+    ]
 
-                with open(pinn_values_name, 'r') as f:
-                    pinn_values = f.read()
-                    pinn_values = pinn_values.replace('\n', '')
-                    pinn_values = pinn_values.replace('[','')
-                    pinn_values = pinn_values.replace(']','')
-                    pinn_values = pinn_values.split(',')
-                    
-                    pinn_values = np.array([float(num) for num in pinn_values if num != ''], dtype=float)
+    for file_with_display_path in files_with_display_paths:
+        for loss_name in [
+                # "loss_fn_basic",
+                "loss_fn_strong",
+                "loss_fn_weak",
+                "loss_fn_weak_and_strong"
+            ]:
+            with open(f"./display_paths/{file_with_display_path}") as file1:
+                file = file1.read()
+                
+                file = file.replace("\\","/")
+                files = file.split('\n')
+                for file in files:
+                    test_function_name = os.path.join(file, loss_name, "test_functions_values.txt")
+                    domain_name = os.path.join(file, loss_name, "x.txt")
+                    time = os.path.join(file, loss_name, "time.txt")
+                    parameters = os.path.join(file, loss_name, "other_parameters.txt")
 
-                with open(domain_name) as f:
-                    x_values = f.read()
-                    x_values = x_values.split(',')[:-1]
-                    x_values = np.array([float(elem) for elem in x_values])
-                    y_values = np.linspace(0,1,len(x_values))
+                    with open(test_function_name, 'r') as f:
+                        test_function_values = f.read()
+                        test_function_values = test_function_values.replace('\n', ' ')
+                        test_function_values = test_function_values.replace('[','')
+                        test_function_values = test_function_values.replace(']','')
+                        test_function_values = test_function_values.replace(',', ' ')
+                        test_function_values = test_function_values.split()
+                        test_function_values = np.array([float(num) for num in test_function_values if num != ''], dtype=float)
+
+                    with open(domain_name) as f:
+                        x_values = f.read()
+                        x_values = x_values.split(',')[:-1]
+                        x_values = np.array([float(elem) for elem in x_values])
+                        y_values = np.linspace(0,1,len(x_values))
 
 
-                with open(time) as t:
-                    t = t.read()
-                    t = t.replace(".","_")
+                    with open(time) as t:
+                        t = t.read()
+                        t = t.replace(".","_")
 
-                with open(parameters) as par:
-                    par = par.read()
-                    par = par.split(",")
-                    par = par[4]
-                    par = par.split()[1]
-                    par = par.replace(".", "")
+                    with open(parameters) as par:
+                        par = par.read()
+                        par = par.split(",")
+                        par = par[4]
+                        par = par.split()[1]
+                        par = par.replace(".", "")
 
-                X, Y = np.meshgrid(x_values, y_values)
-                pinn_values = pinn_values.reshape((len(X), len(Y)))
-                print("Points shape: ", x_values.shape)
-                print("Used loss function: ", loss_name)
-                print("Time: ", t)
-                print("Epsilon: ", par)
+                    X, Y = np.meshgrid(x_values, y_values)
+                    test_function_values = test_function_values.reshape((len(X), len(Y)))
+                    print("Points shape: ", x_values.shape)
+                    print("Used loss function: ", loss_name)
+                    print("Time: ", t)
+                    print("Epsilon: ", par)
 
-                # Create the figure and axes
-                fig = plt.figure()
-                ax = fig.add_subplot(projection='3d')
+                    # Create the figure and axes
+                    fig = plt.figure()
+                    ax = fig.add_subplot(projection='3d')
 
-                # Plot the 3D surface
-                ax.plot_surface(X, Y, pinn_values, cmap='viridis')
+                    # Plot the 3D surface
+                    ax.plot_surface(X, Y, test_function_values, cmap='viridis')
 
-                # Set labels and title
-                ax.set_xlabel('x')
-                ax.set_ylabel('y')
-                ax.set_zlabel('u')
-                ax.set_zlim(0,1)
-                ax.set_title('The solution of the Eriksson-Johnson problem')
+                    # Set labels and title
+                    ax.set_xlabel('x')
+                    ax.set_ylabel('y')
+                    ax.set_zlabel('u')
+                    ax.set_zlim(0,1)
+                    ax.set_title('Optimal test function')
 
-                # Show the plot
-                plt.show()
-                # file_with_display_path = file_with_display_path[:-4]
-                # file_with_display_path = file_with_display_path.replace("\\", "/")
-                # file_with_display_path = file_with_display_path + "/"
-                print(file_with_display_path)
-                save_path = os.path.join("C:/Users/patry/OneDrive/Pulpit/Studia_II_stopien/Magisterka/Results/", file_with_display_path[:-4], f"{loss_name}_x{x_values.shape[0]}_eps{par}_time{t}.png")
-                fig.savefig(save_path)
+                    # Show the plot
+                    plt.show()
+                    # file_with_display_path = file_with_display_path[:-4]
+                    # file_with_display_path = file_with_display_path.replace("\\", "/")
+                    # file_with_display_path = file_with_display_path + "/"
+                    print(file_with_display_path)
+                    save_path = os.path.join("C:/Users/patry/OneDrive/Pulpit/Studia_II_stopien/Magisterka/Results/", file_with_display_path[:-4], f"{loss_name}_x{x_values.shape[0]}_eps{par}_time{t}.png")
+                    fig.savefig(save_path)
 
